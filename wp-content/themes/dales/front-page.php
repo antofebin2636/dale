16d1cf0509674a76c99375a7eb29b81cba0754c1
Antony <antofebin@gmail.com>
2021-07-03 08:17:47 +0100

    txtBox

--- wp-content/themes/dales/front-page.php
+++ wp-content/themes/dales/front-page.php
@@ -1,3 +1,5 @@
 <?php include (locate_template ('header.php') ); ?>
 <?php include (locate_template( 'snippets/banner.php')) ?>
+<?php include (locate_template( 'snippets/txtBox.php')) ?>
+<?php include (locate_template( 'snippets/actionBox.php')) ?>
 <?php include (locate_template ('footer.php') ); ?>
